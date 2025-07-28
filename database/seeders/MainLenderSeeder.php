ceb7d11906522ef7b918d4d42de53f7fac0ab2ea
codecraftwt <codecraftwt@gmail.com>
2025-07-28 19:17:40 +0530

    bug fixing

--- database/seeders/MainLenderSeeder.php
+++ database/seeders/MainLenderSeeder.php
@@ -17,90 +17,116 @@ class MainLenderSeeder extends Seeder
             [
                 'lender_name'             => 'Dynamoney',
                 'lender_logo'            => 'dynamoney.png',
+                'GST_registration'     => 'Yes'
             ]
         );
         MainLenderTable::create(
             [
                 'lender_name'             => 'Shift',
                 'lender_logo'            => 'shift.png',
+                'GST_registration'     => 'No'
+
             ]
         );
         MainLenderTable::create(
             [
                 'lender_name'             => 'LUMI',
                 'lender_logo'            => 'lumi.png',
+                'GST_registration'     => 'No'
+
             ]
         );
         MainLenderTable::create(
             [
                 'lender_name'             => 'Moula',
                 'lender_logo'            => 'moula.png',
+                'GST_registration'     => 'Yes'
             ]
         );
         MainLenderTable::create(
             [
                 'lender_name'             => 'Propsa',
                 'lender_logo'            => 'propsa.png',
+                'GST_registration'     => 'No'
+
             ]
         );
         MainLenderTable::create(
             [
                 'lender_name'             => 'On Deck',
                 'lender_logo'            => 'on_deck.png',
+                'GST_registration'     => 'No'
+
             ]
         );
         MainLenderTable::create(
             [
                 'lender_name'             => 'Boost Business',
                 'lender_logo'            => 'boost_business.png',
+                'GST_registration'     => 'No'
+
             ]
         );
         MainLenderTable::create(
             [
                 'lender_name'             => 'Finance One',
                 'lender_logo'            => 'finance_one.png',
+                'GST_registration'     => 'Yes'
             ]
         );
         MainLenderTable::create(
             [
                 'lender_name'             => 'Finance One Plus',
                 'lender_logo'            => 'finance_one_plus.jpg',
+                'GST_registration'     => 'Yes'
             ]
         );
         MainLenderTable::create(
             [
                 'lender_name'             => 'Bizcap LOC',
                 'lender_logo'            => 'bizcap_loc.png',
+                'GST_registration'     => 'No'
+
             ]
         );
         MainLenderTable::create(
             [
                 'lender_name'             => 'Bizcap',
                 'lender_logo'            => 'bizcaploc.png',
+                'GST_registration'     => 'No'
+
             ]
         );
         MainLenderTable::create(
             [
                 'lender_name'             => 'Capify',
                 'lender_logo'            => 'capify.png',
+                'GST_registration'     => 'No'
+
             ]
         );
         MainLenderTable::create(
             [
                 'lender_name'             => 'Skyecap',
                 'lender_logo'            => 'Skyecap.png',
+                'GST_registration'     => 'No'
+
             ]
         );
         MainLenderTable::create(
             [
                 'lender_name'             => 'Banjo Business Loan Express',
                 'lender_logo'            => 'Banjo_Business_Loan_Express.png',
+                'GST_registration'     => 'No'
+
             ]
         );
         MainLenderTable::create(
             [
                 'lender_name'             => 'MONEYTECH',
                 'lender_logo'            => 'moneytech.png',
+                'GST_registration'     => 'No'
+
             ]
         );
     }
